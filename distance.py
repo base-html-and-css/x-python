e8ad799cff6d1cab22e8c9dcab2b520e8510c297
Nikita Ruban <53084330+base-html-and-css@users.noreply.github.com>
2024-09-30 21:04:57 +0300

    Update distance.py

--- distance.py
+++ distance.py
@@ -7,7 +7,6 @@ def dis():
         yards = feet * 0.333333333
         miles = feet * 0.000189393939
 
-        # Оновлюємо текстові поля результатів
         i.config(text=f"Дюйми: {inches}")
         y.config(text=f"Ярди: {yards}")
         m.config(text=f"Милі: {miles}")
@@ -20,15 +19,12 @@ root.geometry("400x300")
 label1 = Label(root, text="Конвертатор з футів", font=("Arial", 16))
 label1.pack(pady=10)
 
-# Поле для введення футів
 entry = Entry(root, width=10, font=("Arial", 14))
 entry.pack(pady=10)
 
-# Кнопка для запуску конвертації
 button = Button(root, text="ТИЦЬ", command=dis, font=("Arial", 14))
 button.pack(pady=10)
 
-# Поля для відображення результатів
 i = Label(root, text="", font=("Arial", 12))
 i.pack()
 
